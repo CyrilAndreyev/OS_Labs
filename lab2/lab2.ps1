516a586a38cb80d0af1262f2b1b939d60ccf3ca7
CyrilAndreyev <cyrilandreyev@icloud.com>
2021-03-13 17:10:39 +0300

    draft

---
+++ lab2/lab2.ps1
@@ -0,0 +1,30 @@
+﻿Get-Process chrome
+
+Get-Process | Where-Object {$_.ProcessName -eq 'chrome'}
+Get-Process | Where-Object {$_.ProcessName.Contains('sm')}
+Get-Process | Where-Object {$_.Id -eq '2200'}
+
+Stop-Process -Name notepad
+
+"Hello, World!" > "C:\Temp\FirstWayToSave.txt"
+"Hello, World!" | Out-File "C:\Temp\SecondWayToSave.txt"
+
+Get-Date -Format "yyyyMMdd_HHmmss"
+
+Get-ChildItem "C:\Temp" | Where-Object {$_.Length -eq 8} | Remove-Item
+
+While($True)
+{
+    $cDateTime = Get-Date -Format "ss"
+    Write-Host "CurrentTime $($cDateTime)"
+    Sleep(1)
+    if($cDateTime -eq 10)
+    {
+        break
+    }
+}
+
+Get-Process | Where-Object {$_.ProcessName.Contains('a')} | Select-Object Id, ProcessName | Export-Csv "C:\Temp\APList.csv" -NoTypeInformation
+ps | ? {$_.ProcessName.Contains('a')} | select Id, ProcessName | epcsv "C:\Temp\APList.csv" -NoTypeInformation
+
+Get-Alias
