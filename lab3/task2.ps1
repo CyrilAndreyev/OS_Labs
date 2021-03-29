89ddd2b99426c3b64bc1891acbe8e2110c1f45b7
CyrilAndreyev <cyrilandreyev@icloud.com>
2021-03-29 20:58:55 +0300

    [lab3] task2

---
+++ lab3/task2.ps1
@@ -0,0 +1,38 @@
+﻿<#
+  .SYNOPSIS
+  Writes running and stopped services to separate files.
+
+  .DESCRIPTION
+  Writes running and stopped services to separate files: one file for running services and one for stopped services.
+#>
+
+gsv | ? {$_.Status -eq "Running"} > C:\Temp\running-services.txt
+gsv | ? {$_.Status -eq "Stopped"} > C:\Temp\stopped-services.txt
+
+
+function Get-ServiceStatus {
+
+<#
+  .SYNOPSIS
+  Returns service status.
+
+  .DESCRIPTION
+  Returns service status (Running or Stopped).
+
+  .PARAMETER ServiceName
+  Specifies the service name of service to be retrieved. Wildcards are permitted.
+
+  .INPUTS
+  None. You cannot pipe objects to Get-ServiceStatus.
+
+  .OUTPUTS
+  This function returns service status.
+#>
+
+    param (
+        $ServiceName
+    )
+
+    (gsv $ServiceName).Status | Write-Output
+
+}
