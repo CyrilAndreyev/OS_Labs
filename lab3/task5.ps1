78748068c44d900275900fcf9a8ec3d5beaeab89
CyrilAndreyev <cyrilandreyev@icloud.com>
2021-03-29 23:01:01 +0300

    [lab3] task5

---
+++ lab3/task5.ps1
@@ -0,0 +1,15 @@
+﻿<#
+  .SYNOPSIS
+  Creates Windows Scheduled Task.
+
+  .DESCRIPTION
+  Creates Windows Scheduled Task. The task starts at the scheduled time.
+#>
+
+$A = New-ScheduledTaskAction -Execute "notepad.exe"
+$T = New-ScheduledTaskTrigger -Once -At 5pm
+$S = New-ScheduledTaskSettingsSet
+
+$D = New-ScheduledTask -Action $A -Trigger $T -Settings $S
+
+Register-ScheduledTask T1 -InputObject $D
