6aa281fdeef25222bdf160514fe700c80e952b95
CyrilAndreyev <cyrilandreyev@icloud.com>
2021-03-29 22:09:14 +0300

    [lab3] task4

---
+++ lab3/task4.ps1
@@ -0,0 +1,11 @@
+﻿<#
+  .SYNOPSIS
+  Calls Sysinternal application and writes its output to file.
+
+  .DESCRIPTION
+  Calls "PsInfo" Sysinternal tool and writes application's output to a text file.
+#>
+
+Invoke-Command -ScriptBlock {
+    Downloads\SysinternalsSuite\PsInfo.exe
+} > C:\Temp\PsInfo.txt
